35902d28fb1cb2cea419a44bbd1331baebe753a5
Sebastian Wiktor <sebastian.wiktor@pollub.edu.pl>
2023-10-03 16:29:25 +0200

    Initial Commit

---
+++ LAB1_swift/LAB1_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  LAB1_swiftApp.swift
+//  LAB1_swift
+//
+//  Created by student on 03/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct LAB1_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
